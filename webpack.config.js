9b793145062bb43f8b3e5ad27bc8ba1cded93104
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-09-11 02:42:56 -0500

    chore: optimize webpack output
    - Install terser-webpack-plugin
    - Use webpack env prop
    - Move option entry to webpack file
    - Split vendors chunks
    - Minify output with terser

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,80 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
-const prod = process.env.NODE_ENV === "production";
 
 function src(subdir) {
   return path.join(__dirname, "src", subdir);
 }
 
-module.exports = {
-  devtool: prod ? undefined : "source-map",
-  mode: prod ? "production" : "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
-          options: {
-            configFile: "tsconfig.prod.json",
+module.exports = (env) => {
+  const ENV = env.ENV ?? "development";
+
+  return {
+    devtool: ENV === "production" ? undefined : "source-map",
+    entry: "./src/index.tsx",
+    mode: ENV,
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader",
+            options: {
+              configFile: "tsconfig.prod.json",
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+          parallel: true,
+        }),
+      ],
+      runtimeChunk: "single",
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: "all",
+            name: "vendors",
+            test: /[\\/]node_modules[\\/]/,
           },
         },
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  output: {
-    path: __dirname + "/dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  resolve: {
-    alias: {
-      assets: src("assets"),
-      components: src("components"),
-      types: src("types"),
-      utils: src("utils"),
-      views: src("views"),
     },
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
+    output: {
+      filename: "[name].[contenthash].js",
+      path: __dirname + "/dist",
+    },
+    performance: {
+      maxAssetSize: 244000,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+    resolve: {
+      alias: {
+        assets: src("assets"),
+        components: src("components"),
+        types: src("types"),
+        utils: src("utils"),
+        views: src("views"),
+      },
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+  };
 };
